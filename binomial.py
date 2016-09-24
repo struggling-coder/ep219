e12a559af4afaec2d40082db002cea03927c43e1
arkyaC <arkya.chat@gmail.com>
2016-09-24 14:06:06 +0530

    added text box

--- binomial.py
+++ binomial.py
@@ -7,4 +7,5 @@ n=100
 for i in range(n+1):
     prob=math.factorial(n)/(math.factorial(i)*math.factorial(n-i))
     plt.plot([i,i],[0,prob],'r')
+plt.figtext(0.76,.8,'n=%d\np=%4.2f' %(n,p), fontsize=14 ,bbox=dict(facecolor='red', alpha=0.5))
 plt.show()
