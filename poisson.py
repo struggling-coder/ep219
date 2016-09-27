d3505bd99ae0309ecc3a3e227ca9f6788ddfc565
arkyaC <arkya.chat@gmail.com>
2016-09-27 23:08:39 +0530

    generated plots

--- poisson.py
+++ poisson.py
@@ -3,11 +3,10 @@ import matplotlib.mlab as mlab
 import numpy as np
 import math
 
-l=10 #parameter lambda
+l=60 #parameter lambda
 for i in range(0, 100):
     prob=math.exp(-l) * math.pow(l,i) / math.factorial(i) #evaluating value at i
-    plt.plot([i,i],[0,prob],'r') #plotting graph
+    plt.plot([i,i],[0,prob],'b') #plotting graph
 #textbox and graph display
-plt.figtext(0.79,.8,'$lambda=%d$' %(l), fontsize=14 ,bbox=dict(facecolor='red', alpha=0.1),horizontalalignment='center',verticalalignment='center')
+plt.figtext(0.79,.8,'$\lambda=%d$' %(l), fontsize=14 ,bbox=dict(facecolor='red', alpha=0.1),horizontalalignment='center',verticalalignment='center')
 plt.show()
-
