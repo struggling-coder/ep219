1b32801367999492eab33c8b57572ec76cdf8c07
Aditya Malusare <aditya.malusare@gmail.com>
2016-10-06 13:32:42 +0530

    Completed expansion.py
    Most of it, anyways

---
+++ Week 2 Code/expansion.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import numpy as np
+import math
+
+# Define data path and initialize variables
+DATA = './linearexpansion.csv'
+data = {}; num = 0; den = 0.0
+
+# Define mean function
+def _mean(l):
+	return sum(l) / len(l)
+
+# Load data
+for e in file.read(open(DATA)).split('\r\n'):
+	d = e.split(",")
+	data[float(d[0])] = float(d[1])
+	plt.scatter(float(d[0]), float(d[1]), s=1)
+
+# Calculate best fit line
+xm = _mean(data.keys()); ym = _mean(data.values())
+for w in data:
+	num += (w - xm) * (data[w] - ym)
+	den += (w - xm) ** 2
+slope = num / den; intercept = ym - slope * xm
+
+# Plot best-fit line
+x = np.linspace(0, 15, 300)
+plt.plot(x, slope * x + intercept, label="Coefficient of expansion = "+str(slope))
+
+# Show plot and prediction
+p_length = lope * 15 + intercept
+plt.xlabel("Temperatute in Celsius")
+plt.ylabel("Length in mm")
+plt.plot()
+
+plt.show()
