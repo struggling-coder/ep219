4cc5871cfce4a020f7656109ccb5e53f954bdd54
arkyaC <arkya.chat@gmail.com>
2016-10-12 01:12:33 +0530

    removing some syntax errors, and error in SD formula

--- Week 2 Code/pandas.py
+++ Week 2 Code/pandas.py
@@ -1,15 +1,16 @@
 import math
 
 # Initialize variables and the source of data (pandas.txt)
-DATA = ''
+DATA = './pandas.txt'
 count=0; count2=0; index=0
 
 # Read data from pandas.txt
-for e in file.read(open(DATA)).split('\n'):
+for e_raw in file.read(open(DATA)).split('\r\n'):
+	e=float(e_raw)
 	count+=e; count2+=e*e; index+=1
 
 # Calculate and display the required output
-mean = count / index
-sd = math.sqrt(count2 / index)
+mean = count / index # mean
+sd = math.sqrt( count2 / index - mean ** 2 ) # standard deviation
 print "Mean weight: "+str(mean)+"kg\nError on the mean weight: ??"
 print "Typical fluctuation about mean: "+str(sd)+"kg"
