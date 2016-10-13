dacbf6a3d8328d0981992756b96be1de13e2178f
arkyaC <arkya.chat@gmail.com>
2016-10-13 14:28:01 +0530

    final changes added

--- Week 2 Code/expansion.py
+++ Week 2 Code/expansion.py
@@ -18,11 +18,20 @@ for e in file.read(open(DATA)).split('\r\n'):
 	plt.scatter(float(d[0]), float(d[1]), s=1)
 
 # Calculate best fit line
+# Slope of best fit line is given by:
+# slope = sum of ((xi - xm) * (yi - ym)) / ((xi - xm) ** 2) over all i
 xm = _mean(data.keys()); ym = _mean(data.values())
 for w in data:
 	num += (w - xm) * (data[w] - ym)
 	den += (w - xm) ** 2
 slope = num / den; intercept = ym - slope * xm
+best_fit_error = 0
+for w in data:
+	best_fit_error += (data[w] - (slope * w + intercept)) ** 2 # calculate the best fit error
+best_fit_error = math.sqrt(best_fit_error / len(data))
+print "Best estimate for length of the rod at 0 degrees Celsius is %5.2f mm" %(intercept)
+print "Coefficient of linear expansion is %5.2f mm/K" %(slope)
+print "Error on prediction: %5.2f mm" %(best_fit_error)
 
 # Plot best-fit line
 x = np.linspace(0, 15, 300)
--- Week 2 Code/pandas.py
+++ Week 2 Code/pandas.py
@@ -2,15 +2,16 @@ import math
 
 # Initialize variables and the source of data (pandas.txt)
 DATA = './pandas.txt'
-count=0; count2=0; index=0
+count=0; count2=0; index=0; fluctuation=0; values=[]
 
 # Read data from pandas.txt
 for e_raw in file.read(open(DATA)).split('\r\n'):
-	e=float(e_raw)
+	e=float(e_raw); values.append(e)
 	count+=e; count2+=e*e; index+=1
 
 # Calculate and display the required output
 mean = count / index # mean
 sd = math.sqrt( count2 / index - mean ** 2 ) # standard deviation
-print "Mean weight: "+str(mean)+"kg\nError on the mean weight: ??"
-print "Typical fluctuation about mean: "+str(sd)+"kg"
+fluctuation = sum([abs(e - mean) / index for e in values])
+print "Mean weight: %5.2f kg\nError on the mean weight: %5.2f kg" %(mean, sd)
+print "Typical fluctuation about mean: %5.2f kg" %(fluctuation)
