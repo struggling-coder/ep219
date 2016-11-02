df65c6ba79684dd812dc638fb216b663561bd826
arkyaC <arkya.chat@gmail.com>
2016-11-03 01:28:03 +0530

    m,c error intervals evaluated

--- Week 3 Code/mle.py
+++ Week 3 Code/mle.py
@@ -63,15 +63,16 @@ xsum = xm * len(data)
 ysum = ym * len(data)
 
 #plotting the error ellipse
-x = np.linspace(slope - .5, slope + .5, 1000)
-y = np.linspace(intercept - .5, intercept + .5, 1000)
+x = np.linspace(slope - 5, slope + 5, 1000)
+y = np.linspace(intercept - 20, intercept + 20, 1000)
 x, y = np.meshgrid(x, y)
 
-#coefficients for 1sigma error ellipse
-A, H, B, G, F, C = xsq, 2*xsum, len(data), -2*xy, -2*ysum, ysq - (best_fit_error ** 2)*len(data)
+#coefficients for n-sigma error ellipse
+sigmasq = best_fit_error ** 2
+A, H, B, G, F, C = xsq, 2*xsum, len(data), -2*xy, -2*ysum, ysq - sigmasq*len(data)
 assert H**2 - 4*A*B < 0
 
-CS = ax2.contour(x, y,(A*x**2 + H*x*y + B*y**2 + G*x + F*y + C), levels=[1,2], colors=['r','b'])
+CS = ax2.contour(x, y,(A*x**2 + H*x*y + B*y**2 + G*x + F*y + C), levels=[sigmasq,2*sigmasq], colors=['r','b'])
 ax2.set_xlabel('m')
 ax2.set_ylabel('c')
 #making the legend
@@ -79,4 +80,24 @@ labels = ['1-sigma', '2-sigma']
 for i in range(len(labels)):
     CS.collections[i].set_label(labels[i])
 ax2.legend(loc='upper left')
+
+#for m error interval
+coeff = []
+coeff.append(A)
+coeff.append(H*intercept + G)
+coeff.append(B*intercept**2 + F*intercept + C - sigmasq)
+m12 = np.roots(coeff)
+m1 = min(m12[0],m12[1])
+m2 = max(m12[0],m12[1])
+print ("Error interval for m is [%5.2f,%5.2f]")%(m1,m2)
+#for c error interval
+coeff = []
+coeff.append(B)
+coeff.append(H*slope + F)
+coeff.append(A*slope**2 + G*slope + C - sigmasq)
+c12 = np.roots(coeff)
+c1 = min(c12[0],c12[1])
+c2 = max(c12[0],c12[1])
+print ("Error interval for c is [%5.2f,%5.2f]")%(c1,c2)
+
 plt.show()
